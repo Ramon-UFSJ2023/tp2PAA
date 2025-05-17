32556d5fb8debe65fa0e3ade8e28ccfce9ab6a07
Ramon Caique De Paula <132967661+Ramon-UFSJ2023@users.noreply.github.com>
2025-05-17 12:01:46 -0300

    Construindo a Leitura do txt

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "structs.h"
 
 
 int main(){
-    int instancias=0;
+    int instancias=0, maxCitys=0, maxDist=0, maxWeight=0, maxPaths=0, cityNow=0;
     scanf("%d", &instancias);
+    FILE *arq = fopen("entrada.txt", "r");
 
     while(instancias > 0){
-
+        scanf("%d %d %d %d", &maxCitys, &maxDist, &maxWeight, &maxPaths);
+        City povos[maxCitys];
+        for(int i =0; i<maxCitys;i++){
+            scanf("%d", &cityNow);
+            scanf("%d", &povos[cityNow].Weight_Soldier);
+            scanf("%d", &povos[cityNow].Hability);
+        }
+        
 
         instancias--;
     }
--- structs.h
+++ structs.h
@@ -2,10 +2,6 @@
 #define STRUCTS_H
 #include <stdio.h>
 
-    typedef struct Nave{
-        int Max_Weight;
-    }Nave;
-
     typedef struct City{
         int Weight_Soldier;
         int Hability;
