ffdaf4bc689bbc9dbd0f60b169d74acf1b624c13
Ramon Caique De Paula <132967661+Ramon-UFSJ2023@users.noreply.github.com>
2025-05-24 13:15:09 -0300

    .

--- progMod.c
+++ progMod.c
@@ -1,8 +1,23 @@
 #include "progMod.h"
+int maximo(int a, int b){return a>b ? a : b;}
+#define INACESSIVEL (INT_MIN/2)
 
-void functionProgMod(City *povos, MatAdj *cidadePovosDist, int maxPeso, int max_Dist){
-    int *melhorCaminho;
-    for(int i=0; i<cidadePovosDist->rows_columns;i++){ //para que eu teste começando de todas as cidades (rows_columns equivale ao numero de cidades)
+void functionProgMod(City *povos, MatAdj *cidadePovosDist, int maxPeso, int max_Dist, int* habilidadeMaxima, int *melhorCaminho){
+    int totalDistancias= max_Dist+1;
+    int totalPeso =maxPeso+1;
 
+    int ***tabelaDinamica = malloc(cidadePovosDist->rows_columns*sizeof(int**));
+    if(tabelaDinamica == NULL){habilidadeMaxima = 0; return;}
+    
+    for(int cidadeAtual=0; cidadeAtual<cidadePovosDist->rows_columns; cidadeAtual++){
+        for(int distPercorrida = 0; distPercorrida<totalDistancias;distPercorrida++){
+            tabelaDinamica[cidadeAtual][distPercorrida] = malloc((totalPeso)*sizeof(int));
+            for(int pesoCarr=0; pesoCarr<totalPeso;pesoCarr++){
+                tabelaDinamica[cidadeAtual][distPercorrida][pesoCarr] = INACESSIVEL;
+            }
+        }
     }
+    for(int cidades =0; cidades<cidadePovosDist->rows_columns;cidades++) tabelaDinamica[cidades][0][0];
+
+    
 }
